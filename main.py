87839363fd2730034bd8800c7023c031fb7eedd2
Takumi Fuyuki <fuyuki0121ta@gmail.com>
2024-06-29 16:55:25 +0900

    upload.html

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 # main.py
 
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template
+from google.cloud import storage
+import os
 
 app = Flask(__name__)
 
+# Google Cloud Storageのクライアントを初期化
+storage_client = storage.Client()
+bucket_name = 'your-bucket-name'
+bucket = storage_client.bucket(bucket_name)
+
 @app.route('/')
 def hello():
-    return 'Hello, World! This is a Flask main running on Google main Engine.'
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(port=8080)
