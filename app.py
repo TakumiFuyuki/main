db71496cdd9944ed05e934b03b0839aeb31d6fd5
Takumi Fuyuki <fuyuki0121ta@gmail.com>
2024-06-30 17:30:59 +0900

    send

--- app.py
+++ app.py
@@ -44,24 +44,24 @@ def list_files():
 
     return render_template('list_files.html', files=file_list)
 
-# @app.route('/download/<filename>')
-# def download_file(filename):
-#     blob = bucket.blob(filename)
-#     file_path = os.path.join('/tmp', filename)
-#     blob.download_to_filename(file_path)
-#     return send_file(file_path, as_attachment=True)
-
 @app.route('/download/<filename>')
 def download_file(filename):
-    # ダウンロード対象のファイルのblobオブジェクトを取得
     blob = bucket.blob(filename)
+    file_path = os.path.join('/tmp', filename)
+    blob.download_to_filename(file_path)
+    return send_file(file_path, as_attachment=True)
 
-    # ダウンロード用の一時URLを生成する
-    expiration = datetime.now() + timedelta(hours=1)  # 現在時刻から1時間有効
-    signed_url = blob.generate_signed_url(
-        version="v4",
-        expiration=expiration,
-        method="GET",
+# @app.route('/download/<filename>')
+# def download_file(filename):
+#     # ダウンロード対象のファイルのblobオブジェクトを取得
+#     blob = bucket.blob(filename)
+
+#     # ダウンロード用の一時URLを生成する
+#     expiration = datetime.now() + timedelta(hours=1)  # 現在時刻から1時間有効
+#     signed_url = blob.generate_signed_url(
+#         version="v4",
+#         expiration=expiration,
+#         method="GET",
     )
 
     # 生成したURLをリダイレクトしてダウンロードさせる
