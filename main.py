c76dde57826b738a2b9d8d7961d7f37c546b7f08
Takkun <fuyuki0121ta@gmail.com>
2024-07-02 13:52:28 +0900

    flash無し

--- main.py
+++ main.py
@@ -1,27 +1,55 @@
 # main.py
 
-from flask import Flask, request, redirect, url_for, render_template, send_file
-from google.cloud import storage
+from flask import Flask, request, redirect, url_for, render_template, send_file, flash
+from google.cloud import storage, bigquery
 import os
 from datetime import datetime, timedelta
+import utils
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
 
 # クライアントを初期化し、認証情報を設定します
+bigquery_client = bigquery.Client()
 storage_client = storage.Client()
+
+dataset_name = 'my-project-46138-427502.dataset'
+register_table = 'register'
 bucket_name = 'text_upload'
 bucket = storage_client.bucket(bucket_name)
 
 @app.route('/', methods=['GET'])
 def index():
+
     return render_template('index.html')
 
-@app.route('/register', methods=['GET'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        button_time = datetime.now()
+        email = request.form['email']
+        password = request.form['password']
+        if not utils.is_valid_password(password):
+            flash('パスワードは4文字以上で、アルファベットと数字が少なくとも1文字以上含まれている必要があります。')
+            return redirect(url_for('register'))
+        if utils.is_email_registered(email, dataset_name , register_table, bigquery_client):
+            flash('このメールアドレスはすでに登録されています。')
+            return redirect(url_for('register'))
+        utils.insert_register_to_bigquery(email, button_time, password, dataset_name, register_table, bigquery_client)
+        flash('登録が完了しました。ログインしてください。')
+        return redirect(url_for('login'))
     return render_template('register.html')
 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        if not utils.authenticate_user(email, password, dataset_name, register_table, bigquery_client):
+            flash('メールアドレスかパスワードが異なります。')
+            return redirect(url_for('login'))
+        else:
+            return redirect(url_for('main'))
     return render_template('login.html')
 
 @app.route('/upload', methods=['GET','POST'])
---
+++ utils.py
@@ -0,0 +1,50 @@
+# utils.py
+
+import re
+
+def is_valid_password(password):
+    if len(password) < 4:
+        return False
+    if not re.search('[A-Za-z]', password):
+        return False
+    if not re.search('[0-9]', password):
+        return False
+    return True
+
+def is_email_registered(email, dataset_name , register_table, bigquery_client):
+    query = f"""
+    SELECT COUNT(1) as count FROM `{dataset_name}.{register_table}`
+    WHERE e-mail = '{email}'
+    """
+    query_job = bigquery_client.query(query)
+    results = query_job.result()
+    for row in results:
+        if row['count'] > 0:
+            return True
+    return False
+
+def insert_registration_to_bigquery(email, button_time, password, dataset_name, register_table, bigquery_client):
+    button_time_iso = button_time.isoformat()
+    rows_to_insert = [
+        {
+            'datetime': button_time_iso,
+            'e-mail': email,
+            'password': password
+        }
+    ]
+    table_id = f'{dataset_name}.{register_table}'
+    errors = bigquery_client.insert_rows_json(table_id, rows_to_insert)
+    if errors:
+        raise Exception(f'BigQueryへのデータ挿入中にエラーが発生しました: {errors}')
+
+def authenticate_user(email, password, dataset_name, register_table, bigquery_client):
+    query = f"""
+    SELECT password FROM `{dataset_name}.{register_table}`
+    WHERE e-mail = '{email}'
+    """
+    query_job = bigquery_client.query(query)
+    results = query_job.result()
+    for row in results:
+        if row['password'] == password:
+            return True
+    return False
