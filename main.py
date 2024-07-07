44cb93f8ee3067938b5a3605a48896029a386334
Takumi Fuyuki <fuyuki0121ta@gmail.com>
2024-07-07 15:32:57 +0900

    完成

--- main.py
+++ main.py
@@ -91,33 +91,6 @@ def upload_file():
         return '無効なファイル形式です。'
     return render_template('upload.html')
 
-# @app.route('/list', methods=['GET'])
-# def list_files():
-#     if not session.get('logged_in'):
-#         flash('セッションが切れました')
-#         return redirect(url_for('login'))
-
-#     email = session.get('user')
-
-#     # BigQuery から該当ユーザーのファイルリストを取得
-#     query = f"""
-#         SELECT filename FROM `{dataset_name}.upload_file`
-#         WHERE email = '{email}'
-#     """
-
-#     query_job = bigquery_client.query(query)
-#     results = query_job.result()
-
-#     blobs = bucket.list_blobs()
-#     file_list = []
-#     for blob in blobs:
-#         for row in results:
-#             if blob == row.filename:
-#                 if blob.name.endswith('.txt'):
-#                     file_list.append(blob.name)
-
-#     return render_template('list_files.html', files=file_list)
-
 @app.route('/list', methods=['GET'])
 def list_files():
     if not session.get('logged_in'):
